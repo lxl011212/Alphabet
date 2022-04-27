4a8835733d5a47970ebe18dadb31532d2b58d1be
Steven Liu <lxlsteven011212@gmail.com>
2022-04-26 22:24:50 -0400

    refigured env variables

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
   images: {
     domains: ["upload.wikimedia.org"],
   },
   env: {
-    API_KEY: 'apiKey',
-    CONTEXT_KEY: 'contextKey',
-  },
+    API_KEY: process.env.API_KEY,
+    CONTEXT_KEY: process.env.CONTEXT_KEY
+  }
 };
